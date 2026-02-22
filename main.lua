6162b308a76b7e041626e5bcaf291dda773363b9
numerouno2 <129433263+numerouno2@users.noreply.github.com>
2026-02-22 22:49:48 +0700

    Enhance game loading with PlaceId fallback
    Added fallback mechanism to check for PlaceId if GameId is not found.

--- main.lua
+++ main.lua
@@ -1,7 +1,21 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/numerouno2/eugunewupremium/refs/heads/main/localgame.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAMEKAMU/REPOKAMU/main/GameList.lua"))()
 
 local URL = Games[game.GameId]
 
 if URL then
-  loadstring(game:HttpGet(URL))()
+    loadstring(game:HttpGet(URL))()
+else
+    -- Game tidak terdaftar
+    local gameId = tostring(game.GameId)
+    local placeId = tostring(game.PlaceId)
+
+    -- Coba pakai PlaceId juga
+    URL = Games[game.PlaceId]
+    if URL then
+        loadstring(game:HttpGet(URL))()
+    else
+        warn("❌ Game tidak terdaftar!")
+        warn("GameId: " .. gameId)
+        warn("PlaceId: " .. placeId)
+    end
 end
