aeaa7a022588c8181e80c020ef8da03e16dd3559
numerouno2 <129433263+numerouno2@users.noreply.github.com>
2026-02-23 17:04:13 +0700

    Add new game URL for Ruang Riung

--- localgame.lua
+++ localgame.lua
@@ -1,5 +1,6 @@
 local Games = {
   [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo", -- Sawah Indo
+  [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua", -- Ruang Riung
 }
 
 return Games
