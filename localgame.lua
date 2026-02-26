8da2c8611d14ae0193d9710ca26f999275176f7e
numerouno2 <129433263+numerouno2@users.noreply.github.com>
2026-02-27 04:31:09 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -3,6 +3,7 @@ local Games = {
   [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua", -- Ruang Riung
   [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice", -- Indo Voice
   [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo", -- Danau Voice
+  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata", -- Sambung  Kata
 }
 
 return Games
