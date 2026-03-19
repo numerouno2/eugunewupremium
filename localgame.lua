0d1f773a28d2f08ca7997444180b6da09e079841
numerouno2 <129433263+numerouno2@users.noreply.github.com>
2026-03-19 17:00:42 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -1,11 +1,11 @@
 local Games = {
-  [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo.lua",
+  [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo",
   [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua",
-  [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice.lua",
-  [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo.lua",
-  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata.lua",
-  [93978595733734] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ViolenceDistrict.lua",
-  [72774564502867] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Lengkapikata.lua",
-  [78632820802305] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/IndoStrike.lua"
+  [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice",
+  [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo",
+  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata",
+  [93978595733734] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ViolenceDistrict",
+  [72774564502867] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Lengkapikata",
+  [78632820802305] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/IndoStrike"
 }
 return Games
