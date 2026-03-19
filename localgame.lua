f67f66e82035a5ed728d8d6b5db3daf2f9d3c893
numerouno2 <129433263+numerouno2@users.noreply.github.com>
2026-03-19 17:02:50 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -1,11 +1,12 @@
 local Games = {
-  [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo",
-  [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua",
-  [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice",
-  [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo",
-  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata",
-  [93978595733734] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ViolenceDistrict",
-  [72774564502867] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Lengkapikata",
-  [78632820802305] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/IndoStrike"
+  [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo", -- Sawah Indo
+  [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua", -- Ruang Riung
+  [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice", -- Indo Voice
+  [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo", -- Danau Voice
+  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata", -- Sambung  Kata
+  [93978595733734] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ViolenceDistrict", -- Violence District
+  [72774564502867] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Lengkapikata", -- Lengkapi Kata
+  [78632820802305] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/IndoStrike", -- Indo Strike
 }
+
 return Games
