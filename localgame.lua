da59398984e76b2603145f94c0e23a707080f130
numerouno2 <129433263+numerouno2@users.noreply.github.com>
2026-03-19 16:47:38 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -1,12 +1,11 @@
 local Games = {
-  [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo", -- Sawah Indo
-  [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua", -- Ruang Riung
-  [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice", -- Indo Voice
-  [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo", -- Danau Voice
-  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata", -- Sambung  Kata
-  [93978595733734] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ViolenceDistrict", -- Violence District
-  [72774564502867] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Lengkapikata", -- Lengkapi Kata
-  [78632820802305] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/IndoStrike", -- Indo Strike
+  [83369512629707] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/sawahindo.lua",
+  [131848958487439] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ruangriung.lua",
+  [8356562067] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/indovoice.lua",
+  [85695526103771] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/danauindo.lua",
+  [130342654546662] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Sambungkata.lua",
+  [93978595733734] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/ViolenceDistrict.lua",
+  [72774564502867] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/Lengkapikata.lua",
+  [78632820802305] = "https://raw.githubusercontent.com/numerouno2/gamesdump/refs/heads/main/IndoStrike.lua"
 }
-
 return Games
